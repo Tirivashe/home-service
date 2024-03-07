5f4e6c00fd957f89e740655a7adb4a7113551881
Tirivashe <shaymusts@gmail.com>
2024-03-07 12:07:44 +0200

    initial project setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    prependData: `@import "./_mantine.scss";`,
+  },
+};
 
 export default nextConfig;
