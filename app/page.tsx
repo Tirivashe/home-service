23cf7c9e656564ccfda0ec921f789e05ce59bea5
Tirivashe <shaymusts@gmail.com>
2024-03-10 18:41:26 +0200

    added categories section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Categories from "@/components/Categories";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Categories />
     </main>
   );
 }
