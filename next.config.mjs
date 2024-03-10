19fbadcdd7e04451e47b3816ce8d49ec3c89748a
Tirivashe <shaymusts@gmail.com>
2024-03-10 18:15:22 +0200

    image domains accessed by hygraph

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   sassOptions: {
     prependData: `@import "./_mantine.scss";`,
   },
+  images: {
+    domains: ["us-east-1-shared-usea1-02.graphassets.com"],
+  },
 };
 
 export default nextConfig;
