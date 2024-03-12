52aeb8f0d03f4146140ccdc6b8241a08aa531a4a
Tirivashe <shaymusts@gmail.com>
2024-03-12 20:05:07 +0200

    page for categories layout

---
+++ app/(routes)/search/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+import CategorySideNav from "@/components/CategorySideNav";
+import { Grid } from "@mantine/core";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const layout = ({ children }: Props) => {
+  return (
+    <Grid style={{ overflow: "hidden" }} py="xl" px="lg">
+      <Grid.Col span={3} visibleFrom="sm">
+        <CategorySideNav />
+      </Grid.Col>
+      <Grid.Col span="auto">{children}</Grid.Col>
+    </Grid>
+  );
+};
+
+export default layout;
