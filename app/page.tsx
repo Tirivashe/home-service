8de3e440287045dad9a0c9ce9b85bac8f07f8006
Tirivashe <shaymusts@gmail.com>
2024-03-12 20:04:12 +0200

    added padding to the home businesses list

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import BusinessList from "@/components/BusinessList";
 import Categories from "@/components/Categories";
 import Hero from "@/components/Hero";
+import { Box } from "@mantine/core";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Categories />
-      <BusinessList />
+      <Box p="xl">
+        <BusinessList />
+      </Box>
     </main>
   );
 }
