459298161673b628b40cc3db1dbb47d40e3adb94
Tirivashe <shaymusts@gmail.com>
2024-03-15 18:31:19 +0200

    can now change booking status and visual updates

--- app/(routes)/booking-history/_components/BookingHistoryList/index.tsx
+++ app/(routes)/booking-history/_components/BookingHistoryList/index.tsx
@@ -1,10 +1,30 @@
-import { GetUserBookingHistoryQuery } from "@/services/queries";
-import { Group, Image, SimpleGrid, Stack, Text } from "@mantine/core";
-import { IconCalendar, IconClock, IconMapPin, IconUser } from "@tabler/icons-react";
+import { GetUserBookingHistoryQuery, UpdateBookingStatusMutation } from "@/services/queries";
+import {
+  Badge,
+  Button,
+  Group,
+  Image,
+  Modal,
+  Select,
+  SimpleGrid,
+  Stack,
+  Text,
+  rem,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { notifications } from "@mantine/notifications";
+import {
+  IconCalendar,
+  IconCheck,
+  IconClock,
+  IconMapPin,
+  IconUser,
+  IconX,
+} from "@tabler/icons-react";
 import { useSession } from "next-auth/react";
 import NextImage from "next/image";
-import React from "react";
-import { useQuery } from "urql";
+import React, { useState } from "react";
+import { useMutation, useQuery } from "urql";
 
 type Props = {
   status: "Booked" | "Canceled" | "Completed";
@@ -12,63 +32,164 @@ type Props = {
 
 const BookingHistoryList = ({ status }: Props) => {
   const { data } = useSession();
+  const [opened, { open, close }] = useDisclosure(false);
+  const [newStatus, setNewStatus] = useState<string | null>("");
+  const [bookingId, setBookingId] = useState<string | number>("");
 
   const [{ data: userBookings }] = useQuery({
     query: GetUserBookingHistoryQuery,
     variables: { userEmail: data?.user?.email, status },
     pause: !data,
   });
+
+  const [_, updateBookingMutation] = useMutation(UpdateBookingStatusMutation);
+
+  const handleOpenModal = (id: string | number) => {
+    setBookingId(id);
+    open();
+  };
+
+  const updateBooking = async () => {
+    close();
+    const updateMutationNotifID = notifications.show({
+      title: "Please wait...",
+      message: "We are updating your booking status...",
+      loading: true,
+      withBorder: true,
+      autoClose: false,
+    });
+
+    try {
+      const result = await updateBookingMutation({
+        id: bookingId,
+        status: newStatus as "Booked" | "Completed" | "Canceled",
+      });
+
+      if (result.error) {
+        notifications.update({
+          id: updateMutationNotifID,
+          color: "red",
+          loading: false,
+          message: "Something went wrong, we could not change your booking.",
+          title: "Error",
+          autoClose: 3000,
+          withCloseButton: false,
+          icon: <IconX style={{ width: rem(18), height: rem(18) }} />,
+        });
+        console.error(result.error.message);
+        return;
+      }
+
+      if (result.data?.updateBooking?.id) {
+        notifications.update({
+          id: updateMutationNotifID,
+          color: "teal",
+          loading: false,
+          message: "Your booking status has been changed successfully.",
+          title: "Success!",
+          autoClose: 3000,
+          withCloseButton: false,
+          withBorder: true,
+          icon: <IconCheck style={{ width: rem(18), height: rem(18) }} />,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <SimpleGrid cols={{ base: 1, sm: 2 }} py="lg">
-      {userBookings?.bookings?.map((booking) => (
-        <Group
-          key={booking.id}
-          p="xs"
-          gap="sm"
-          grow
-          preventGrowOverflow
-          style={{ borderRadius: "1rem", border: "1px solid var(--mantine-color-gray-5)" }}
-        >
-          <Image
-            component={NextImage}
-            src={booking.businessList?.images[0].url}
-            alt="image"
-            height={150}
-            width={150}
-            radius="md"
+    <>
+      <SimpleGrid cols={{ base: 1, sm: 2 }} py="lg">
+        {userBookings?.bookings?.map((booking) => (
+          <Group
+            key={booking.id}
+            p="xs"
+            gap="sm"
+            preventGrowOverflow
+            style={{ borderRadius: "1rem", border: "1px solid var(--mantine-color-gray-5)" }}
+          >
+            <Group>
+              <Image
+                component={NextImage}
+                src={booking.businessList?.images[0].url}
+                alt="image"
+                height={150}
+                width={150}
+                radius="md"
+              />
+            </Group>
+            <Stack flex={1} gap="0.5rem" style={{ flex: 1, overflow: "hidden" }}>
+              <Text size="sm" fw="bold">
+                {booking.businessList?.name}
+              </Text>
+              <Group gap="sm" style={{ overflow: "hidden" }}>
+                <IconUser size="1rem" style={{ color: "var(--mantine-color-logo-orange-5)" }} />
+                <Text c="logo-orange.5" size="xs">
+                  {booking.businessList?.contactPerson}
+                </Text>
+              </Group>
+              <Group gap="sm" w="100%" style={{ overflow: "hidden" }}>
+                <IconMapPin size="1rem" style={{ color: "var(--mantine-color-logo-orange-5)" }} />
+                <Text size="xs" truncate="end">
+                  {booking.businessList?.address}
+                </Text>
+              </Group>
+              <Group gap="xs">
+                <IconCalendar size="1rem" style={{ color: "var(--mantine-color-logo-orange-5)" }} />
+                <Text size="xs" truncate="end">
+                  Service On: <strong>{booking.date}</strong>
+                </Text>
+              </Group>
+              <Group gap="sm">
+                <IconClock size="1rem" style={{ color: "var(--mantine-color-logo-orange-5)" }} />
+                <Text size="xs" truncate="end">
+                  Service At: <strong>{booking.time}</strong>
+                </Text>
+              </Group>
+              {status === "Booked" && (
+                <Button onClick={() => handleOpenModal(bookingId)} size="compact-xs">
+                  Change Status
+                </Button>
+              )}
+              {status === "Completed" && (
+                <Badge size="sm" color="green">
+                  Completed
+                </Badge>
+              )}
+              {status === "Canceled" && (
+                <Badge size="sm" color="red">
+                  Canceled
+                </Badge>
+              )}
+            </Stack>
+          </Group>
+        ))}
+      </SimpleGrid>
+      <Modal
+        title="Change Status"
+        opened={opened}
+        onClose={close}
+        overlayProps={{
+          backgroundOpacity: 0.55,
+          blur: 3,
+        }}
+        centered
+      >
+        <Stack gap="sm" justify="center">
+          <Select
+            label="What would you like to your booking to?"
+            placeholder="Pick New Booking Status"
+            data={["Completed", "Canceled"]}
+            value={newStatus}
+            onChange={setNewStatus}
           />
-          <Stack gap="xs" style={{ flex: 1, overflow: "hidden" }}>
-            <Text size="sm" fw="bold">
-              {booking.businessList?.name}
-            </Text>
-            <Group gap="sm" style={{ overflow: "hidden" }}>
-              <IconUser size="1rem" style={{ color: "var(--mantine-color-logo-orange-5)" }} />
-              <Text c="logo-orange.5" size="xs">
-                {booking.businessList?.contactPerson}
-              </Text>
-            </Group>
-            <Group gap="sm" w="100%" style={{ overflow: "hidden" }}>
-              <IconMapPin size="1rem" style={{ color: "var(--mantine-color-logo-orange-5)" }} />
-              <Text size="xs" truncate="end">
-                {booking.businessList?.address}
-              </Text>
-            </Group>
-            <Group gap="xs">
-              <IconCalendar size="1rem" style={{ color: "var(--mantine-color-logo-orange-5)" }} />
-              <Text size="xs" truncate="end">
-                Service On: <strong>{booking.date}</strong>
-              </Text>
-            </Group>
-            <Group gap="sm">
-              <IconClock size="1rem" style={{ color: "var(--mantine-color-logo-orange-5)" }} />
-              <Text size="xs" truncate="end">
-                Service At: <strong>{booking.time}</strong>
-              </Text>
-            </Group>
-          </Stack>
-        </Group>
-      ))}
-    </SimpleGrid>
+          <Button fullWidth variant="subtle" onClick={updateBooking}>
+            Change Status
+          </Button>
+        </Stack>
+      </Modal>
+    </>
   );
 };
 
