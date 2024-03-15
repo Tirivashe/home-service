22e78f64078f7e63ece56a10c66425784a6395ae
Tirivashe <shaymusts@gmail.com>
2024-03-15 10:31:15 +0200

    added notifications to completed or errored booking mutation

--- app/(routes)/details/[businessId]/page.tsx
+++ app/(routes)/details/[businessId]/page.tsx
@@ -4,7 +4,7 @@ import {
   GetBusinessByIdQuery,
   GetSimilarBusinessByIdQuery,
 } from "@/services/queries";
-import { Center, Container, Grid, Loader } from "@mantine/core";
+import { Center, Container, Grid, Loader, rem } from "@mantine/core";
 import { signIn, useSession } from "next-auth/react";
 import { useParams } from "next/navigation";
 import React from "react";
@@ -13,6 +13,9 @@ import Info from "../_components/Info";
 import styles from "./styles.module.scss";
 import SimilarBusinesses from "../_components/SimilarBusinesses";
 import Description from "../_components/Description";
+import { Notifications, notifications } from "@mantine/notifications";
+import { IconCheck } from "@tabler/icons-react";
+import { IconX } from "@tabler/icons-react";
 
 type Props = {};
 
@@ -33,9 +36,15 @@ const BusinessDetails = (props: Props) => {
     },
   });
 
-  const [createBookingResult, createBookingMutation] = useMutation(CreateBookingMutation);
+  const [_, createBookingMutation] = useMutation(CreateBookingMutation);
 
   const createBooking = async (date: string, time: string) => {
+    const createBookingStatus = notifications.show({
+      title: "Please wait...",
+      message: "We are booking your appointment for you.",
+      loading: true,
+      withBorder: true,
+    });
     try {
       const result = await createBookingMutation({
         id: businessId,
@@ -45,10 +54,32 @@ const BusinessDetails = (props: Props) => {
         userName: data?.user?.email,
       });
       if (result.error) {
-        console.log(result.error);
+        notifications.update({
+          id: createBookingStatus,
+          color: "red",
+          loading: false,
+          message: "Something went wrong, we could not book your appointment.",
+          title: "Error",
+          autoClose: 3000,
+          withCloseButton: false,
+          icon: <IconX style={{ width: rem(18), height: rem(18) }} />,
+        });
+        console.error(result.error.message);
         return;
       }
-      alert(result.data?.createBooking?.id); // TODO: implement proper notification fxn
+      if (result.data?.createBooking?.id) {
+        notifications.update({
+          id: createBookingStatus,
+          color: "teal",
+          loading: false,
+          message: "Your booking has been successfully created.",
+          title: "Success!",
+          autoClose: 3000,
+          withCloseButton: false,
+          withBorder: true,
+          icon: <IconCheck style={{ width: rem(18), height: rem(18) }} />,
+        });
+      }
     } catch (error) {
       console.error(error);
     }
@@ -69,6 +100,7 @@ const BusinessDetails = (props: Props) => {
   if (status === "authenticated") {
     return (
       <Container size="responsive" className={styles.root} p="lg" mt="xl">
+        <Notifications />
         <Info business={business?.businessList} />
         <Grid py="xl" mt="lg">
           <Grid.Col span={{ base: 12, md: 9 }}>
