8d9cfb7bc98c54c3389edcfd4794068c1492d830
Tirivashe <shaymusts@gmail.com>
2024-03-15 10:25:00 +0200

    mantine notifications styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
 import "./globals.scss";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
+import "@mantine/notifications/styles.css";
 
 import {
   AppShell,
