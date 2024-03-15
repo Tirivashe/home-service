9039fd4e9f94fb8aa462be62fe3864d4f5c87916
Tirivashe <shaymusts@gmail.com>
2024-03-15 10:23:10 +0200

    updated time slots

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -4,20 +4,32 @@ export function capitalize(str: string) {
 
 export const getTimeSlots = () => {
   const timeList = [];
-  for (let i = 8; i <= 12; i++) {
+  for (let i = 8; i <= 17; i++) {
+    if (i === 12) {
+      timeList.push({
+        time: "12:00 PM",
+      });
+      timeList.push({
+        time: "12:30 PM",
+      });
+      continue;
+    }
+
+    if (i > 12) {
+      timeList.push({
+        time: i - 12 + ":00 PM",
+      });
+      timeList.push({
+        time: i - 12 + ":30 PM",
+      });
+      continue;
+    }
+
     timeList.push({
       time: i + ":00 AM",
     });
     timeList.push({
       time: i + ":30 AM",
-    });
-  }
-  for (let i = 1; i <= 5; i++) {
-    timeList.push({
-      time: i + ":00 PM",
-    });
-    timeList.push({
-      time: i + ":30 PM",
     });
   }
 
